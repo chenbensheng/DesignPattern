257a4ee52696ab5e79ccdabc00ff1dd0df06023b
chenbensheng <chenbensheng@vipkid.com.cn>
2020-02-06 10:50:57 +0800

    提交

---
+++ src/main/java/com/atguigu/singleton/Singleton.java
@@ -0,0 +1,20 @@
+package com.atguigu.singleton;
+
+public class Singleton {
+
+    private volatile static Singleton instance;
+    private Singleton(){};
+
+    public static Singleton getInstance() {
+
+        if(null==instance){
+            synchronized (Singleton.class){
+                if(null==instance){
+                    instance=new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+}
